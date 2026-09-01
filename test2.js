d50179884ad0fa7b0d4816840e74708e6798734c
UR Rehman <umair.rehman221@gmail.com>
2026-09-01 16:58:57 +0400

    Implement guessing game function

---
+++ test2.js
@@ -0,0 +1,13 @@
+function guessingGame() {
+  const magicNumber = 22;
+  const guess = prompt('guess a number between 1 and 100!');
+  if (guess > magicNumber) {
+    alert('YOUR GUESS IS TOO BIG');
+  } else if (guess < magicNumber) {
+    alert('YOUR GUESS IS TOO SMALL');
+  } else if (guess == magicNumber) {
+    alert('YOU DID IT! 🎉');
+  } else {
+    return 'INVALID INPUT';
+  }
+}
